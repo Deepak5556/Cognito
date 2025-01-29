a2fe8b2227072b63d251c345c72fa75336616ac5
Deepak <deepakviji5556@gmail.com>
2025-01-29 23:35:27 +0530

    fixed sidebar button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { icons } from '../assets/icons'
 import "../style/Slidebar.css";
-import SideBarButton from './SideBarButton';
+// import SideBarButton from './SideBarButton';
 
 const Sidebar = () => {
     const { RiBook2Fill, RxDashboard, IoBookOutline, BiMessageRoundedDetail, FaBrain, BiSolidBookReader, IoSettings,

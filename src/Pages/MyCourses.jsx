99f50fae9ca5ca16791f254d8b53cfff3bf10782
Deepak <deepakviji5556@gmail.com>
2025-02-23 10:24:50 +0530

    Course secction

--- src/Pages/MyCourses.jsx
+++ src/Pages/MyCourses.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const MyCourses = () => {
   return (
     <div>
--- src/components/CoursesList.jsx
+++ src/components/CoursesList.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import CoursesBox from './CoursesBox';
+import deepak from '../assets/img/Deepak.png'
 
 const CoursesList = () => {
   const courses = [
-    { title: 'ReactJS From Scratch - Basic', rating: 4.3, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'ReactJS With Tailwind CSS - Basic', rating: 4.7, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'Next.js From Scratch - Basic', rating: 4.6, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'ReactJS From Scratch - Basic', rating: 4.3, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'ReactJS With Tailwind CSS - Basic', rating: 4.7, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'Next.js From Scratch - Basic', rating: 4.6, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'ReactJS From Scratch - Basic', rating: 4.3, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'ReactJS With Tailwind CSS - Basic', rating: 4.7, instructor: 'Dr. Antrim Vijay', price: 500 },
-    { title: 'Next.js From Scratch - Basic', rating: 4.6, instructor: 'Dr. Antrim Vijay', price: 500 }
+    { title: 'ReactJS From Scratch - Basic', rating: 4.3, instructor: 'Dr. Antrim Vijay', price: 500 , thumbnail:deepak },
+    { title: 'ReactJS With Tailwind CSS - Basic', rating: 4.7, instructor: 'Dr. Antrim Vijay', price: 800 ,thumbnail:deepak},
+    { title: 'Next.js From Scratch - Basic', rating: 4.6, instructor: 'Dr. Antrim Vijay', price: 500,thumbnail:deepak },
+    { title: 'ReactJS From Scratch - Basic', rating: 4.3, instructor: 'Dr. Antrim Vijay', price: 800,thumbnail:deepak },
+    { title: 'ReactJS With Tailwind CSS - Basic', rating: 4.7, instructor: 'Dr. Antrim Vijay', price: 1500,thumbnail:deepak },
+    { title: 'Next.js From Scratch - Basic', rating: 4.6, instructor: 'Dr. Antrim Vijay', price: 500,thumbnail:deepak },
+    { title: 'ReactJS From Scratch - Basic', rating: 4.3, instructor: 'Dr. Antrim Vijay', price: 500,thumbnail:deepak },
+    { title: 'ReactJS With Tailwind CSS - Basic', rating: 4.7, instructor: 'Dr. Antrim Vijay', price: 500,thumbnail:deepak },
+    { title: 'Next.js From Scratch - Basics', rating: 4.6, instructor: 'Dr. Antrim Vijay', price: 500 ,thumbnail:deepak}
   ];
 
   return (
@@ -21,10 +22,13 @@ const CoursesList = () => {
           {courses.map((course, index) => (
             <div className="col-md-4" key={index}>
               <CoursesBox
+
                 title={course.title}
                 rating={course.rating}
                 instructor={course.instructor}
                 price={course.price}
+                thumbnail={course.thumbnail}
+
               />
             </div>
           ))}

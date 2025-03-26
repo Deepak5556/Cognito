ba9be318d4c09992edbbcc60458ab53079e8fd77
Deepak <deepakviji5556@gmail.com>
2025-03-26 11:04:13 +0530

    Backend

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const a =10;
-console.log(a);
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./database/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Server is Working");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+  connectDB();
+});

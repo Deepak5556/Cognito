beadfa8dfd07f75a4e79dece2aa121703ad74ac0
Deepak <deepakviji5556@gmail.com>
2025-06-14 00:24:37 +0530

    Just Update

--- database/db.js
+++ database/db.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB,{});
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database Connected");
   } catch (error) {
-    console.log(error);
+    console.error("DB Connection Error:", error);
+    process.exit(1);
   }
 };
